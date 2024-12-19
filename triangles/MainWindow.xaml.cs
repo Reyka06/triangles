7da2abc7f6a4e95ce9f1e1d943e4cc3999b53686
Reyka06 <114016980+Reyka06@users.noreply.github.com>
2024-12-19 05:50:03 +0300

    Add files via upload

---
+++ triangles/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace triangles
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void DetermineTriangleType_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(sideA.Text, out double a) &&
+               double.TryParse(sideB.Text, out double b) &&
+               double.TryParse(sideC.Text, out double c))
+            {
+                string triangleType;
+                if (a == b && b == c)
+                {
+                    triangleType = "Результат: Равносторонний треугольник";
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    triangleType = "Результат: Равнобедренный треугольник";
+                }
+                else
+                {
+                    triangleType = "Результат: Разносторонний треугольник";
+                }
+
+                result.Text = triangleType;
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные значения для всех сторон.");
+            }
+        }
+    }
+}
